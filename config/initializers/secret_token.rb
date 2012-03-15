bcf3b15bdce20db560d2e344fe67a929351568c5
John Gesimondo <john@jmondo.com>
2012-03-14 21:16:12 -0400

    Initial project commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Classtrack4::Application.config.secret_token = '969f2dd191f634a758583e209fe77fcd21cc730f0fc276940a77c3d500b3bccf4b09768482db2cf47e5b025fc1d7637fd3ea89aef15e453911fd42d5fe637b11'
