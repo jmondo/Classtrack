54c7a59d446b9cc019bc2a53a05fc398a1b47072
John Gesimondo <john@jmondo.com>
2012-04-03 00:18:53 -0400

    fix hosts again

--- config/initializers/hosts.rb
+++ config/initializers/hosts.rb
@@ -1,7 +1,7 @@
 configatron.default_host = {
   :development => "localhost:3000",
   :test => "localhost:3000",
-  :production => "classtrackit.com",
+  :production => "classtrackit.com"
 }[Rails.env.to_sym]
 
 ActionMailer::Base.default_url_options[:host] = configatron.default_host
