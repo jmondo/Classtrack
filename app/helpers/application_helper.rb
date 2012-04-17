bd885cfc88db63c9cf5b4a498e61269df6e05ca5
John Gesimondo <john@jmondo.com>
2012-04-16 21:05:43 -0400

    style flashes with bootstrap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,14 @@ module ApplicationHelper
   def body_class
     "#{controller.controller_name} #{controller.controller_name}-#{controller.action_name}"
   end
+
+  def bootstrap_flash_key(key)
+    new_key = case key
+    when :alert
+      'alert-error'
+    when :notice
+      'alert-success'
+    end
+    new_key
+  end
 end
